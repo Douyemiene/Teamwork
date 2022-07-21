65e2b84114219097f8cb55d677e8f72276989e2f
douyemiene <viktormiene.vm@gmail.com>
2022-07-21 21:33:35 +0100

    feat: update user schema

--- src/Infrastructure/mongo/models/user.ts
+++ src/Infrastructure/mongo/models/user.ts
@@ -15,14 +15,22 @@ export interface IUser {
 
 const UserSchema = new mongoose.Schema<IUser>({
     id: String,
-    username: String,
-    email: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
     password: String,
     fullname: String,
     bio: String,
     following: Number,
     followers: [String]
-},
+    },
     { timestamps: true });
 
 export const User = mongoose.model<IUser>("User", UserSchema);
