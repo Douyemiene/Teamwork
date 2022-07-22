5ea7cd26ec98ae3ffd54ef735e921758347df7e2
douyemiene <viktormiene.vm@gmail.com>
2022-07-22 23:06:24 +0100

    feat: userModel to container

--- src/container.ts
+++ src/container.ts
@@ -1,5 +1,7 @@
 import { Container } from 'inversify'
-import { MongoDBConnection } from './Infrastructure/mongo/config';
+import { MongoDBConnection } from './infrastructure/mongo/config';
+import { Model } from 'mongoose';
+import { User, IUser } from './infrastructure/mongo/models/user'
 
 
 // create container
@@ -8,4 +10,6 @@ let container = new Container({ autoBindInjectable: true });
 //set up bindings
 container.bind<Function>('db').toConstantValue(MongoDBConnection);
 
+//models
+container.bind<Model<IUser>>('userModel').toConstantValue(User)
 export default container
