30429faed5e4f7081c24a4ab4e1feab2af106f23
douyemiene <viktormiene.vm@gmail.com>
2022-07-24 21:27:40 +0100

    feat: use httpContext in user controller

--- src/http/controllers/user.ts
+++ src/http/controllers/user.ts
@@ -1,19 +1,23 @@
 import * as express from "express";
-import { interfaces, controller, httpGet, httpPost, httpDelete, request, queryParam, response, requestParam, next } from "inversify-express-utils";
+import { interfaces, controller, httpGet, httpPost, request, response, next, HttpResponseMessage, StringContent, BaseHttpController } from "inversify-express-utils";
 import { injectable, inject } from "inversify";
 import { UserRepository } from "../../repositories/user";
 import { SignUp } from "../../application/auth/signUp";
 
 @controller("/user")
-export class UserController implements interfaces.Controller {
+export class UserController extends BaseHttpController  {
 
-    constructor(private userRepository: UserRepository, private signUp: SignUp,  ) { }
+    constructor( private signUp: SignUp ) { 
+        super()
+    }
 
     @httpGet("/")
-    private index(@request() req: express.Request, @response() res: express.Response, @next() next: express.NextFunction) {
-        //const result = this.userRepository.create({username: 'da', email:'a@ab.com'})
-        const result = this.signUp.execute({username: 'test5',email:'test@test5.com'})
-        return result
+    private async index(@request() req: express.Request, @response() res: express.Response, @next() next: express.NextFunction) {
+        const {username,email} = this.httpContext.request.body;
+        const result = await this.signUp.execute({username,email})
+         const response = new HttpResponseMessage(200);
+
+        return this.ok(result)
     }
 
  
