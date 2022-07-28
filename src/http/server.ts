018a050c4afc217ed6c1a15431992b2e43f3246e
douyemiene <viktormiene.vm@gmail.com>
2022-07-28 20:38:30 +0100

    feat: swagger

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,13 +3,15 @@ import 'dotenv/config'
 import 'reflect-metadata'
 import * as bodyParser from 'body-parser';
 import { Container } from 'inversify'
+import * as swaggerUI from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
 import { interfaces, InversifyExpressServer, TYPE } from 'inversify-express-utils';
 import '../http/controllers'
 
-export class RestServer{
+export class RestServer {
     private server
-    
-    constructor(container){
+
+    constructor(container) {
         // set up container
         container = new Container({ autoBindInjectable: true });
 
@@ -20,18 +22,39 @@ export class RestServer{
                 extended: true
             }));
 
+            const options = {
+                definition: {
+                    openapi: "3.0.0",
+                    info: {
+                        title: "instagram clone API",
+                        version: "1.0.0",
+                        description: "A minimal instagram clone",
+                    },
+                    servers: [
+                        {
+                            url: "http://localhost:30001",
+                        },
+                    ],
+                },
+                apis: [`./src/http/controllers/*.ts`],
+            };
+
+            const specs = swaggerJsDoc(options);
+
             app.use(bodyParser.json());
+
+            app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
         });
     }
 
-    start(){
-        
+    start() {
+
         const app = this.server.build()
 
         const { PORT } = process.env
         app.listen(PORT, () => {
             console.log(`running on http://localhost:${PORT}`)
-            })
+        })
     }
 }
 
