2f2f65e7215fb075fb42be7c6250b283dd50ade6
douyemiene <viktormiene.vm@gmail.com>
2022-08-02 16:31:53 +0100

    feat: remove swagger

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,9 +3,7 @@ import 'dotenv/config'
 import 'reflect-metadata'
 import * as bodyParser from 'body-parser';
 import { Container } from 'inversify'
-import * as swaggerUI from 'swagger-ui-express'
-import swaggerJsDoc from 'swagger-jsdoc'
-import { interfaces, InversifyExpressServer, TYPE } from 'inversify-express-utils';
+import { InversifyExpressServer } from 'inversify-express-utils';
 import '../http/controllers'
 
 export class RestServer {
@@ -22,28 +20,7 @@ export class RestServer {
                 extended: true
             }));
 
-            const options = {
-                definition: {
-                    openapi: "3.0.0",
-                    info: {
-                        title: "instagram clone API",
-                        version: "1.0.0",
-                        description: "A minimal instagram clone",
-                    },
-                    servers: [
-                        {
-                            url: "http://localhost:30001",
-                        },
-                    ],
-                },
-                apis: [`./src/http/controllers/*.ts`],
-            };
-
-            const specs = swaggerJsDoc(options);
-
             app.use(bodyParser.json());
-
-            app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
         });
     }
 
